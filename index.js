fbdfe2ff542463f4a0de5efb4b1d51ad1fc55a6f
Alexander Achberger <achb@hotmail.de>
2016-11-25 00:26:29 +0100

    In index.html linechart begonnen zu integrieren

--- index.js
+++ index.js
@@ -65,7 +65,17 @@ app.get('/gauge', function(req, res) {
   });
   
 app.post('/linechart', function(req, res) { 
+	
 	console.log("get a post");
+	//res.sendFile(html_dir + 'gauge.html');
+	//res.redirect("/gauge.html");
+	//res.sendFile(html_dir + 'linechart.html');
+	res.redirect("/linechart.html");
+}); 
+
+app.get('/getlinechart', function(req, res) { 
+	
+	console.log("get linechart");
 	//res.sendFile(html_dir + 'gauge.html');
 	//res.redirect("/gauge.html");
 	res.sendFile(html_dir + '/images/gauge.png');
